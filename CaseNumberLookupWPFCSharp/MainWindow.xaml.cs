9fd061e92191a54550e8bf4bc3ca79bcc9271228
Douglas Hernández <ddhernandezf@gmail.com>
2023-10-18 11:14:35 -0600

    Douglas Donaldo Hernández Figueroa Test on 10/18/2023

--- CaseNumberLookupWPFCSharp/MainWindow.xaml.cs
+++ CaseNumberLookupWPFCSharp/MainWindow.xaml.cs
@@ -1,29 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.ObjectModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CaseNumberLookupWPFApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            NumberViewModel model = new NumberViewModel(this.addButton, this.ascButton, this.descButton);
 
+            model.numbers = new ObservableCollection<int>() { 2, 23, 12, 44 };
+            this.DataContext = model;
         }
     }
 }
---
+++ CaseNumberLookupWPFCSharp/NumberViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CaseNumberLookupWPFApp
+{
+    public class NumberViewModel : INotifyPropertyChanged
+    {
+        private NumberModel model { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public string input { get; set; }
+        public ObservableCollection<int> numbers { get; set; }
+
+        public NumberViewModel(Button addButton, Button ascButton, Button descButton)
+        {
+            this.model = new NumberModel();
+            addButton.Click += add;
+            ascButton.Click += orderASC;
+            descButton.Click += orderDESC;
+        }
+
+        public void add(object sender, RoutedEventArgs e)
+        {
+            try {
+                this.validate();
+                int.TryParse(this.input, out int number);
+                this.numbers.Add(number);
+                OnPropertyChanged(name: "numbers");
+                this.clearInupt();
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void orderASC(object sender, RoutedEventArgs e)
+        {
+            this.model.data = this.numbers.ToList();
+            this.numbers = this.model.Sort();
+            OnPropertyChanged(name: "numbers");
+        }
+
+        public void orderDESC(object sender, RoutedEventArgs e)
+        {
+            this.model.data = this.numbers.ToList();
+            this.numbers = this.model.Reverse();
+            OnPropertyChanged(name: "numbers");
+        }
+
+        private void validate()
+        {
+            if (string.IsNullOrEmpty(this.input) || string.IsNullOrWhiteSpace(this.input))
+                throw new Exception("Please fill the input");
+
+            if (!int.TryParse(this.input, out int number))
+                throw new Exception("Numbers only");
+        }
+
+        private void clearInupt()
+        {
+            this.input = string.Empty;
+            OnPropertyChanged(name: "input");
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
